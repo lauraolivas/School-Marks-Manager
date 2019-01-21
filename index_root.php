90e3f835f93d0c4be5ba014b8c8d7cb016ab39d1
lauraolivas <39761335+lauraolivas@users.noreply.github.com>
2019-01-21 20:11:13 +0100

    Update index_root.php

--- index_root.php
+++ index_root.php
@@ -28,9 +28,8 @@
   <body id="page-top">
     <?php
     session_start();
-
+    
     if(!empty($_SESSION['user'])&&$_SESSION['type']=='root'){
-      require ('./mysqli_connect.php');
       
     ?>
     <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
@@ -87,7 +86,7 @@
             <a class="dropdown-item" href="#">Settings</a>
             <a class="dropdown-item" href="#">Activity Log</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+            <a class="dropdown-item" data-toggle="modal" data-target="#logoutModal">Logout</a>
           </div>
         </li>
       </ul>
@@ -218,7 +217,7 @@
           <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
           <div class="modal-footer">
             <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <input type="submit" class="btn btn-primary" value="Logout">
+            <a class="btn btn-danger text-white" href="logout.php">Logout</a>
           </div>
         </div>
       </div>
