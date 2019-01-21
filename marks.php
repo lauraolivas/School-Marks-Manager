6f07239ca6f711cc5fa77fd9fdfca7fbac01370c
lauraolivas <39761335+lauraolivas@users.noreply.github.com>
2019-01-21 11:04:59 +0100

    Add files via upload

--- marks.php
+++ marks.php
@@ -31,15 +31,32 @@
         require ('./mysqli_connect.php');
         if(!empty($_SESSION['user'])&&$_SESSION['type']='teacher'){
             
-            
+            $evaluations=['First evaluation','Second evaluation','Third evaluation'];
+            foreach($evaluations as $ev){
+                $eoption="<option value='".$ev."'";
+                if(isset($_POST['evaluation'])){
+                    $eoption.='selected=selected';
+                }
+                $eoption.=">".$ev."</option>";
+            }
+
+            if(!empty($_GET['subject'])){
+                $subject=mysqli_real_escape_string($dbc, trim($_GET['subject']));
+            }
+
             $kq = "SELECT name FROM tasktypes";
             $kr =  @mysqli_query ($dbc, $kq);
             $krecs = @mysqli_fetch_array ($kr, MYSQLI_NUM);
             //echo count($krecs);
             //print_r($krecs);
             for($o=0;$o<count($krecs);$o++){
-                $option="<option>".$krecs[$o]."</option>";
+                $option="<option value='$krecs[$o]'";
+                if (isset($_POST['size'])){
+                    $option.='selected="selected"';
+                }
+                $option.=">$krecs[$o]</option>";
             }
+
             $display = 20;
             if (isset($_GET['p']) && is_numeric($_GET['p'])) { 
                 $pages = $_GET['p'];
@@ -48,18 +65,22 @@
                 $ir = @mysqli_query ($dbc, $iq);
                 $irecs = @mysqli_fetch_array ($ir, MYSQLI_NUM);
                 $records = $irecs[0];
+
                 if ($records > $display) { 
                     $pages = ceil ($records/$display);
                 } else {
                     $pages = 1;
                 }
             }
+
             if (isset($_GET['s']) && is_numeric($_GET['s'])) {
                 $start = $_GET['s'];
             }else{
                 $start = 0;
             }
+
             $sort = (isset($_GET['sort'])) ? $_GET['sort'] : 'ln';
+
             switch ($sort) {
                 case 'fn':
                     $order_by = 'firstname ASC';
@@ -72,8 +93,10 @@
                     $sort = 'ln';
                     break;
             }
-            $tableq = "SELECT firstname,lastname FROM users WHERE type='student' ORDER BY $order_by LIMIT $start, $display";		
+
+            $tableq = "SELECT u.firstname,u.lastname FROM users as u inner join subjects_users as su on u.user=su.user WHERE u.type='student' and su.subjectid='$subject' ORDER BY $order_by LIMIT $start, $display";		
             $tabler = @mysqli_query ($dbc, $tableq); 
+
             $theader='<thead>
                 <tr>
                     <th><a href="diego.php?sort=fn">First name</th>
@@ -81,7 +104,9 @@
                     <th>Mark</th>
                 </tr>
             </thead>';
+
         
+
             $tbody='';
             $j=1;
             while ($recs = mysqli_fetch_array ($tabler, MYSQLI_ASSOC)) {
@@ -89,45 +114,66 @@
                 <td>' . $recs['firstname'] . '</td>
                 <td>' . $recs['lastname'] . '</td>
                 <td><input type="text"';
-                if(!empty($merror)){
-                    $tbody.=$merror;
-                } 
-                $tbody.='name="mark'.$j.'" id="mark'.$j.'" class="form-control" value="';
+                $tbody.='name="mark'.$j.'" required="required" id="mark'.$j.'" class="form-control" value="';
                 if(isset($_POST["mark$j"])){
                     $tbody.= $_POST["mark$j"];
                 }
                 $tbody.='"></td>';
                 $j++;
             } 
+
             if ($_SERVER['REQUEST_METHOD']=='POST'){
+
+                $tablemarkq="SELECT u.user FROM users as u inner join subjects_users as su on u.user=su.user WHERE u.type='student' and su.subjectid='$subject'";
+                $tablemarkr = @mysqli_query ($dbc, $tablemarkq);
                 
-                if(empty($POST['description'])){
-                    $desc_error='<p class="text-danger">Rellene el campo de descripción</p>';
-                    $derror='required="required"';
-                }else{
-                    $description = mysqli_real_escape_string($dbc, trim($_POST['description']));
+                if(!empty($_POST['description'])){
+                    $description = mysqli_real_escape_string($dbc, trim($_POST["description"]));
                 }
+                if(!empty($_POST['tasktype'])){
+                    $tasktype = mysqli_real_escape_string($dbc, trim($_POST["tasktype"]));
+                }
+                
+
                 $m=1;
-                while ($recs2 = mysqli_fetch_array ($tabler, MYSQLI_ASSOC)) {
-                    $insq = "INSERT INTO marks VALUES()";
-			        $insr = @mysqli_query ($dbc, $insq);
+                while ($recs2 = mysqli_fetch_array ($tablemarkr, MYSQLI_ASSOC)) {
+                    $mark = mysqli_real_escape_string($dbc, trim($_POST["mark$m"]));
+                    $user=mysqli_real_escape_string($dbc, trim($recs2['user']));
+
+                    $insq = "INSERT INTO marks(`user`,`subjectid`,`taskname`,`description`,`mark`,`evaluation`) VALUES('$user','$subject','$tasktype','$description',$mark,'$evaluation')";
+                    $insr = @mysqli_query ($dbc, $insq);
+                    if($insr){
+                        echo 'bieeeen';
+                    }else{
+                        echo 'joder';
+                    }
                     $m++;
                 }
             }
             mysqli_free_result ($tabler);
             mysqli_close($dbc);
+
+
             if ($pages > 1) {
+
                 $current_page = ($start/$display) + 1;
+
                 if ($current_page != 1) {
                     $previous= '<a href="modifier_delete_student.php?s=' . ($start - $display) . '&p=' . $pages . '&sort=' . $sort . '">&laquo;</a> ';
                 }
+
+
                 if ($current_page != $pages) {
                     $next= '<a href="modifier_delete_student.php?s=' . ($start + $display) . '&p=' . $pages . '&sort=' . $sort . '">&raquo;</a>';
                 }
+
             }
+
             
+
             
         
+
         ?>
 
         <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
@@ -241,38 +287,15 @@
                         <li class="breadcrumb-item active">Professor Name</li>
                     </ol>
 
-                    <!-- Icon Cards-->
-                    <div class="row">
-                        <div class="col-xl-3 col-sm-6 mb-3">
-                            <div class="card text-white bg-primary o-hidden h-100">
-                                <div class="card-body">
-                                    <div class="card-body-icon">
-                                        <i class="fas fa-fw fa-list"></i>
-                                    </div>
-                                    <div class="mr-5">Exams</div>
-                                </div>
-                                <a class="card-footer text-white clearfix small z-1">
-
-                                    <span class="float-left">View Details</span>
-                                    <span class="float-right">
-                                        <i class="fas fa-angle-right"></i>
-                                    </span>
-                                </a>
-
-                            </div>
-                        </div>
-
-                    </div>
-
                     <div class="card mb-3">
                         <div class="card-header">
                             <i class="fas fa-table"></i>
                             Students Table</div>
                         <div class="card-body">
-                            <form method='post' action="">
-                                <?php if(!empty($desc_error)){echo $desc_error;} ?>
+                            <form method='POST' action="">
+                                
                                 <label for="description">Description: </label>
-                                <input type="text" name="description" id="description" <?php if(!empty($derror)){echo $derror;} ?> class="form-control" value="<?php if(isset($_POST["description"])){echo $_POST["description"];}?>">
+                                <input type="text" name="description" id="description" required="required" class="form-control" value="<?php if(isset($_POST["description"])){echo $_POST["description"];}?>">
                                 <br>
 
                                 <label for="tasktype">Task type: </label>
@@ -280,6 +303,15 @@
                                     <?php 
                                         if(!empty($option)){
                                             echo $option;
+                                        }
+                                    ?>
+                                </select><br>
+
+                                <label for="evaluation">Evaluation: </label>
+                                <select class="form-control" id="evaluation" name="evaluation">
+                                    <?php 
+                                        if(!empty($eoption)){
+                                            echo $eoption;
                                         }
                                     ?>
                                 </select><br>
@@ -296,6 +328,7 @@
                                     if(!empty($previous)){
                                         echo $previous;
                                     }
+
                                     if ($pages > 1) {
                                         // Make all the numbered pages:
                                         for ($i = 1; $i <= $pages; $i++) {
@@ -306,6 +339,7 @@
                                             }
                                         } 
                                     }
+
                                     if(!empty($next)){
                                         echo $next;
                                     }
@@ -386,4 +420,4 @@
             
             </body>
 
-</html>
+        </html>
